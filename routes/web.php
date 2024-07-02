fb556603fa3f00b288e34dfa94742f33bd224bfe
Davide Di Giacomo <davidedigiacomo001@gmail.com>
2024-07-02 18:10:14 +0200

    aggiunto il controller

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Controller;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,7 +14,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    $dati = config("data");
-    return view('home', $dati);
-})->name("home");
+Route::get('/', [Controller::class, 'index'])->name("home");
